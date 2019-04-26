22e58ee08b0ceaf12791fcf88872214e2907783c
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-04-26 14:22:32 -0700

    Make tabbed content max width

--- src/components/PlotSelector.vue
+++ src/components/PlotSelector.vue
@@ -1,28 +1,29 @@
 <template>
   <v-layout class="PlotSelector" justify-center>
-    <v-tabs centered light icons-and-text>
-      <v-tabs-slider color="primary"></v-tabs-slider>
-      <v-tab href="#smith">
-        Smith
-        <v-icon>phone</v-icon>
-      </v-tab>
+    <v-flex xs12>
+      <v-tabs fixed-tabs light icons-and-text>
+        <v-tabs-slider color="primary"></v-tabs-slider>
+        <v-tab href="#smith">
+          Smith
+          <v-icon>phone</v-icon>
+        </v-tab>
+        <v-tab href="#cartesian">
+          Rectangular
+          <v-icon>favorite</v-icon>
+        </v-tab>
 
-      <v-tab href="#cartesian">
-        Rectangular
-        <v-icon>favorite</v-icon>
-      </v-tab>
-
-      <v-tab-item value="smith">
-        <v-card flat>
-          <v-card-text>Smith Component Here</v-card-text>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item value="cartesian">
-        <v-card flat>
-          <v-card-text>Cartesian Component Here</v-card-text>
-        </v-card>
-      </v-tab-item>
-    </v-tabs>
+        <v-tab-item value="smith">
+          <v-card flat>
+            <v-card-text>Smith Component Here</v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item value="cartesian">
+          <v-card flat>
+            <v-card-text>Cartesian Component Here</v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </v-flex>
   </v-layout>
 </template>
 
@@ -33,6 +34,6 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.PlotSelector
-  margin: 2em auto
+// .PlotSelector
+//   margin: 2em auto
 </style>
