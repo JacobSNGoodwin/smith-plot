bef5e05e3ccd8a1cdcf972ba9a7aabe8d1f1c568
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-05-01 20:26:04 -0700

    Refactor button to emit file event

--- src/views/PlotView.vue
+++ src/views/PlotView.vue
@@ -1,10 +1,17 @@
 <template>
   <v-container>
-    <FileButton round color="secondary">
+    <FileButton round color="secondary" @file-update="getFiles">
       <h3>Select File</h3>
       <v-icon>add</v-icon>
     </FileButton>
-
+    <v-layout justify-center>
+      <v-flex xs12>
+        <p>
+          File name:
+          <strong>{{currentFile.name}}</strong>
+        </p>
+      </v-flex>
+    </v-layout>
     <PlotSelector/>
   </v-container>
 </template>
@@ -17,6 +24,21 @@ export default {
   components: {
     FileButton,
     PlotSelector
+  },
+  data () {
+    return {
+      currentFile: null
+    }
+  },
+  methods: {
+    getFiles (files) {
+      this.currentFile = files[0]
+    }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+p
+  text-align: center
+</style>
