055e681b29e93f9711551f407719281a7f509bcc
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-05-08 16:16:20 -0700

    Add a basic svg box

--- src/components/plots/SmithPlot.vue
+++ src/components/plots/SmithPlot.vue
@@ -1,8 +1,27 @@
 <template>
   <v-card flat color="accent">
-    <v-card-text>Smith Component Here</v-card-text>
+    <svg :viewBox="svgBox" preserveApectRation="xMidYMid meet"></svg>
   </v-card>
 </template>
+
+<script>
+export default {
+  name: 'SmithPlot',
+  data () {
+    return {
+      margin: 0.025,
+      realLineValues: [0.2, 0.5, 1, 2, 5, 10],
+      imagLineValues: [0.2, 0.5, 1, 2, 5, 10]
+    }
+  },
+  computed: {
+    svgBox () {
+      const totalWidth = 1 + 2 * this.margin
+      return `0 0 ${totalWidth} ${totalWidth}`
+    }
+  }
+}
+</script>
 
 <style lang="stylus" scoped>
 .smith
