22d844f4a4d21e4b400abbca76a03d9c56ae4bc7
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-05-08 17:18:49 -0700

    Plot real circles

--- src/components/plots/SmithPlot.vue
+++ src/components/plots/SmithPlot.vue
@@ -1,16 +1,21 @@
 <template>
-  <v-card flat color="accent">
-    <svg :viewBox="svgBox" preserveApectRation="xMidYMid meet"></svg>
+  <v-card flat>
+    <svg :viewBox="svgBox" preserveApectRation="xMidYMid meet">
+      <g class="realCircles" :transform="groupTranslate">
+        <path v-for="path in realPaths" :key="path" :d="path"></path>
+      </g>
+    </svg>
   </v-card>
 </template>
 
 <script>
+import { getRealPath } from '../../util/smithMath'
 export default {
   name: 'SmithPlot',
   data () {
     return {
       margin: 0.025,
-      realLineValues: [0.2, 0.5, 1, 2, 5, 10],
+      realLineValues: [Number.MIN_VALUE, 0.2, 0.5, 1, 2, 5, 10],
       imagLineValues: [0.2, 0.5, 1, 2, 5, 10]
     }
   },
@@ -18,12 +23,20 @@ export default {
     svgBox () {
       const totalWidth = 1 + 2 * this.margin
       return `0 0 ${totalWidth} ${totalWidth}`
+    },
+    groupTranslate () {
+      return `translate(${this.margin}, ${this.margin})`
+    },
+    realPaths () {
+      return this.realLineValues.map(realValue => getRealPath(realValue))
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-.smith
-  background-color: $accent
+.realCircles
+  stroke: black
+  stroke-width: .005
+  fill: none
 </style>
