34256de60f5515a28935eb5e04e961670b2aa99f
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-05-14 14:32:26 -0700

    Create Expansion List for plots

--- src/components/ui/PlotDrawer.vue
+++ src/components/ui/PlotDrawer.vue
@@ -1,6 +1,21 @@
 <template>
-  <v-navigation-drawer right fixed clipped app v-model="drawer">
-    <h3>A plot drawer</h3>
+  <v-navigation-drawer right fixed clipped app v-model="drawer" disable-resize-watcher>
+    <v-list>
+      <v-list-group v-for="plot in plotsByName" :key="plot.id">
+        <template v-slot:activator>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ plot.name }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-list-tile-title>Plots down here</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list-group>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
@@ -13,8 +28,15 @@ export default {
         return this.$store.state.navDrawer
       },
       set (val) {
-        this.$store.commit('toggleNavDrawer', val)
+        if (val === false) {
+          // this prevents committing to state twice
+          // only to allow for clicking on backdrop in mobile mode
+          this.$store.commit('toggleNavDrawer', val)
+        }
       }
+    },
+    plotsByName () {
+      return this.$store.getters.plotsByName
     }
   }
 }
