280454deab31c3b1ebfdec449859565a5abd2d1b
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-05-24 08:42:48 -0700

    Scaffold out basic Cartesian component

--- src/components/plots/CartesianPlot.vue
+++ src/components/plots/CartesianPlot.vue
@@ -1,5 +1,38 @@
 <template>
-  <v-card flat color="error">
-    <v-card-text>Cartesian Component Here</v-card-text>
+  <v-card flat>
+    <div class="cartesianContainer">
+      <svg class="cartesianSvg" :viewBox="svgBox" preserveApectRation="xMidYMid meet"></svg>
+    </div>
   </v-card>
 </template>
+
+<script>
+export default {
+  name: 'CartesianPlot',
+  props: {
+    plots: Array
+  },
+  data () {
+    return {
+      viewPort: 1000,
+      margin: 25
+    }
+  },
+  computed: {
+    svgBox () {
+      const totalWidth = this.viewPort + 2 * this.margin
+      return `0 0 ${totalWidth} ${totalWidth}`
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.cartesianContainer
+  max-width: 750px
+  margin: auto
+  padding: 2em 0
+
+.cartesianSvg
+  shape-rendering: geometricPrecision
+</style>
